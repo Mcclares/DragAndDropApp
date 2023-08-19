a9bb1c452557bfedbda069a407d13b3855afb0f0
Mcclares <86326552+Mcclares@users.noreply.github.com>
2023-08-19 19:44:46 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+
+
+/// <reference path="components/base-component.ts"/>
+/// <reference path="components/project-input.ts"/>
+/// <reference path="components/project-item.ts"/>
+/// <reference path="components/project-list.ts"/>
+
+namespace App {
+
+
+
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
+
+
+}
---
+++ src/state/project-state.ts
@@ -0,0 +1,74 @@
+namespace App {
+
+ type Listener<T> = (items: T[]) => void;
+
+ class State<T> {
+    protected listeners: Listener<T>[] = [] ;
+    constructor() {
+
+    }
+
+
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn);
+    }
+}
+
+
+//Project State Management
+export class ProjectState extends State<Project> {
+
+   
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+    //Singleton
+    private constructor()
+    {
+        super();
+
+     }
+    
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState;
+        return this.instance;
+    }
+
+ 
+    addProject(title: string, description: string, numOfPeopel: number) {
+        const newProject = new Project(Math.random().toString(), title, description, numOfPeopel, ProjectStatus.Active)
+          
+        this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId)
+        if (project && project.status !==newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+    
+    private updateListeners() {
+         for (const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice());
+        }
+    }
+    }
+
+
+
+
+
+
+   export const projectState = ProjectState.getInstance();
+
+
+
+}
+
+
